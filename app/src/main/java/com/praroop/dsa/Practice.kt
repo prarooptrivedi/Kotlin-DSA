49cd50bd3bf5f791e40b14e91769ca5c8a73716a
PRAROOP <praroop.trivedi149@gmail.com>
2025-08-12 22:21:29 +0530

    Add Gitignore

---
+++ app/src/main/java/com/praroop/dsa/Practice.kt
@@ -0,0 +1,15 @@
+package com.praroop.dsa
+
+import kotlin.math.min
+
+fun main(){
+val arr= arrayOf(1,2,3,4,5,6,7)
+   var n=arr.size
+
+    var newArr=Array(n){0}
+
+    for(i in arr.indices){
+        newArr[i]=arr[n - 1 - i]
+    }
+    println("Reversed Arry ${newArr.joinToString()}")
+}
